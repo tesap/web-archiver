3fc954300e1305d2224df65c998994c7ec0f35ac
Stepan Tsepa <s.tsepa@omp.ru>
2026-02-09 17:53:41 +0300

    [c] Add domain restriction feature

--- c/hrefs_crawler.c
+++ c/hrefs_crawler.c
@@ -7,6 +7,10 @@
 #include <curl/curl.h>
 
 #define REQUEST_PERIOD 50000
+#define RESTRICT_TO_DOMAIN 1
+#define RESTRICT_TO_SUBDOMAIN 2
+
+// #define RESTRICTION_ARG RESTRICT_TO_SUBDOMAIN
 
 struct vec {
     size_t size;
@@ -58,7 +62,59 @@ typedef enum {
     // STATE_CAPTURE,
 } HrefParserState;
 
-void parse_html(const char* url, int depth_level, struct vec* data, void(*callback)(const char*, int, char*)) {
+char* capture_domain_from_url(const char* url) {
+    /*
+     * '.*://<CAPTURE>/.*'
+     */
+
+    int state = 0;
+    struct vec* capture_vec = vec_init(0);
+
+    for (int i = 0; i < strlen(url); i++) {
+        char el = url[i];
+        char* el_ptr = url + i;
+        switch (state) {
+            case 0:
+                if (strncmp(el_ptr, "://", 3) == 0) {
+                    state++;
+                    i += 2;
+                }
+                break;
+            case 1:
+                if (el == '/') {
+                    // Capture END
+                    vec_append(capture_vec, "\0", 1);
+                    goto end_loop;
+                } else {
+                    vec_append(capture_vec, el_ptr, 1);
+                }
+            default:
+        }
+    }
+
+    // Loop ended without goto
+    // fprintf(stderr, "=== capture_domain_from_url: Capture has not reached END\n");
+
+end_loop:; 
+    char* result = capture_vec->ptr;
+    free(capture_vec);
+    return result;
+}
+
+bool ends_with(char* str, char* suffix) {
+    // If the suffix is longer than the string, it cannot be a suffix.
+    int len_suffix = strlen(suffix);
+    int len_str = strlen(str);
+    int delta = len_str - len_suffix;
+
+    if (delta < 0) {
+        return 0;
+    }
+
+    return (strcmp(str + delta, suffix) == 0);
+}
+
+void capture_hrefs_from_html(struct vec* data, const char* page_url, int depth_level, void(*callback)(const char*, int, char*)) {
     /*
      * The function effectively searches for links in HTML document.
      * It uses simple per-byte parser which searches for the following match
@@ -66,6 +122,7 @@ void parse_html(const char* url, int depth_level, struct vec* data, void(*callba
      *      '<a href="<CAPTURE>"'
      * 
      * It calls a @callback function on each captured URL
+     * A caller does not pass ownership of @page_url, he should handle it on its own.
      */
 
     int progress_step = 0;
@@ -119,7 +176,6 @@ void parse_html(const char* url, int depth_level, struct vec* data, void(*callba
                     case 4:
                         if (el == '\"') {
                             progress_step++;
-                            // state = STATE_CAPTURE;
                         } else if (el == ' ' || el == '\n') {
                         } else {
                             state = STATE_DEFAULT;
@@ -129,9 +185,8 @@ void parse_html(const char* url, int depth_level, struct vec* data, void(*callba
                     case 5: // STATE_CAPTURE
                         if (el == '\"') {
                             vec_append(capture_vec, "\0", 1);
-                            (*callback)(url, depth_level, capture_vec->ptr);
+                            (*callback)(page_url, depth_level, capture_vec->ptr);
                             free(capture_vec);
-                            // vec_deinit(capture_vec);
                             capture_vec = vec_init(0);
 
                             state = STATE_DEFAULT;
@@ -169,24 +224,49 @@ bool is_url_http(char* url) {
 
 void crawl_urls(char* url, int depth_level);
 
-void handle_found_url_cb(const char* url, int depth_level, char* found_url) {
+void handle_found_url_cb(const char* page_url, int depth_level, char* found_url) {
+    /*
+     * The function takes ownership of @found_url, i.e. it frees it at the end.
+     */
+    char* result_url;
     if (is_url_http(found_url)) {
-        // Print to STDOUT
-        printf("%s\n", found_url);
-        crawl_urls(strdup(found_url), depth_level - 1);
+        result_url = found_url;
     } else if (is_url_relative(found_url)) {
         char* stripped_found_url = found_url + 1;
-        int full_url_len = strlen(url) + strlen(stripped_found_url) + 1;
+        int full_url_len = strlen(page_url) + strlen(stripped_found_url) + 1;
         char* full_url = malloc(full_url_len * sizeof(char));
         
-        sprintf(full_url, "%s%s", url, stripped_found_url);
-        // Print to STDOUT
-        printf("%s\n", full_url);
-        crawl_urls(full_url, depth_level - 1);
-        // free(full_url);
+        sprintf(full_url, "%s%s", page_url, stripped_found_url);
+        result_url = full_url;
+        free(found_url);
+    } else {
+        // URL does not match needed criterias
+        return;
     }
 
-    free(found_url);
+#ifdef RESTRICTION_ARG
+    char* domain_result_url = capture_domain_from_url(result_url);
+    char* domain_page_url = capture_domain_from_url(page_url);
+#endif
+#if RESTRICTION_ARG == RESTRICT_TO_DOMAIN
+    if (strcmp(domain_page_url, domain_result_url) != 0) {
+        return;
+    }
+#elif RESTRICTION_ARG == RESTRICT_TO_SUBDOMAIN
+    // Check whether result_url is a subdomain of page_url, f.e.:
+    // domain_result_url =  terms.archlinux.org
+    // domain_page_url =          archlinux.org
+    // Then first is a subdomain of the second
+    if (!ends_with(domain_result_url, domain_page_url)) {
+        return;
+    }
+#endif
+
+    // Finally print URL to stdout
+    printf("%s\n", result_url);
+
+    // Crawl URLs recursively
+    crawl_urls(result_url, depth_level - 1);
 }
 
 void debug_libcurl_version() {
@@ -216,7 +296,7 @@ int main(int argc, char* argv[]) {
     const char* errstr;
     int depth_level = atoi(argv[2]);
 
-    debug_libcurl_version();
+    // debug_libcurl_version();
     curl_global_init(CURL_GLOBAL_NOTHING);
 
     crawl_urls(url, depth_level);
@@ -226,12 +306,22 @@ int main(int argc, char* argv[]) {
 }
 
 void crawl_urls(char* url, int depth_level) {
-    usleep(REQUEST_PERIOD); 
-
+    /*
+     * The algorithm is:
+     * - Retrieve requested URL
+     * - Traverse HTML to found http hrefs
+     * - Recursively crawl found URLs by a depth of @depth_level
+     *
+     * A caller should pass ownership of @url, i.e. @url is freed at the end of current function.
+     */
     if (depth_level == 0) {
+        free(url);
         return;
     }
 
+    usleep(REQUEST_PERIOD); 
+
+    // Add '/' at end if missing
     int url_len = strlen(url);
     if (url[url_len-1] != '/') {
         url = realloc(url, url_len + 2);
@@ -256,11 +346,9 @@ void crawl_urls(char* url, int depth_level) {
     if (success != CURLE_OK) {
         fprintf(stderr, "=== Error requesting page\n");
         curl_easy_cleanup(handle);
+        free(url);
         return;
     }
-
-    // long res_status;
-    // curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &res_status);
 
     // Replace url with an effective retrieved by curl.
     char* tmp_url;
@@ -268,7 +356,7 @@ void crawl_urls(char* url, int depth_level) {
     free(url);
     url = strdup(tmp_url);
 
-    parse_html(url, depth_level, html_data, handle_found_url_cb);
+    capture_hrefs_from_html(html_data, url, depth_level, handle_found_url_cb);
 
     curl_easy_cleanup(handle);
     vec_deinit(html_data);
