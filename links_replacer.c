81cfc36d76b4a042115cf9be8a5c63680ece542c
Stepan Tsepa <s.tsepa@omp.ru>
2026-04-09 18:28:10 +0300

    links_replacer: Add path-hint option

--- links_replacer.c
+++ links_replacer.c
@@ -9,11 +9,13 @@
 
 struct cmd_args {
     const char* file_path;
+    const char* path_hint;
     bool in_place;
 };
 
 struct cmd_args cmd_args = {
     .file_path = "",
+    .path_hint = NULL,
     .in_place = false,
 };
 
@@ -21,24 +23,26 @@ void exit_args_error() {
     fprintf(stderr, "Usage: ./links_replacer -f <path> [-i]\n");
     fprintf(stderr, "Options:\n");
     fprintf(stderr, "\t-f --file <path>\tInput file path\n");
+    fprintf(stderr, "\t-h --path-hint <path>\tA hint of where a given file should be located in the root of archive\n");
     fprintf(stderr, "\t-i --in-place\t\tPerform replacement in-place, do not output edited file\n");
     exit(1);
 }
 
 void parse_cmd_args(int argc, char* argv[], struct cmd_args* args) {
     bool f_flag = false;
+    bool h_flag = false;
     bool i_flag = false;
-    bool b_flag = false;
 
     while (1) {
         static struct option long_options[] = {
             {"file",                 required_argument, 0, 'f'},
+            {"path-hint",            required_argument, 0, 'h'},
             {"in-place",             no_argument, 0, 'i'},
             {0, 0, 0, 0}
         };
 
         int longindex;
-        int c = getopt_long(argc, argv, "f:i", long_options, &longindex);
+        int c = getopt_long(argc, argv, "f:h:i", long_options, &longindex);
         if (c == -1) {
             break;
         }
@@ -51,6 +55,13 @@ void parse_cmd_args(int argc, char* argv[], struct cmd_args* args) {
                 }
                 f_flag = strlen(optarg) > 0;
                 args->file_path = optarg;
+                break;
+            case 'h':
+                if (h_flag) {
+                    exit_args_error();
+                }
+                h_flag = strlen(optarg) > 0;
+                args->path_hint = optarg;
                 break;
             case 'i':
                 if (i_flag) {
@@ -80,9 +91,12 @@ int main(int argc, char* argv[]) {
     assert(size > 0);
     assert(buff != NULL);
 
+    char* _path_hint = (cmd_args.path_hint) ? cmd_args.path_hint : cmd_args.file_path;
+    struct vec path_hint = vec_wrap(_path_hint);
+
     replace_links(
         (struct vec){ buff, size },
-        vec_wrap(cmd_args.file_path),
+        path_hint,
         stdout
     );
 }
